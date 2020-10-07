27c2a88198c8a026227d9d48d42583f7bf3a6da3
Matt McMahon <contact@matt-mcmahon.com>
2020-10-06 21:46:51 -0400

    New Build

---
+++ target/node/dist/lib/test-framework.js
@@ -0,0 +1,11 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.assertNotEquals = exports.assertEquals = exports.test = void 0;
+const assert_1 = require("assert");
+Object.defineProperty(exports, "assertEquals", { enumerable: true, get: function () { return assert_1.deepStrictEqual; } });
+Object.defineProperty(exports, "assertNotEquals", { enumerable: true, get: function () { return assert_1.notDeepStrictEqual; } });
+const tap_1 = __importDefault(require("tap"));
+exports.test = (label, implementation) => tap_1.default.test(label, implementation);
