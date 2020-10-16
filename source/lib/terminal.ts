02b2ca8dea787cc3c5dbc9eab2fd466aaef0591a
Matt McMahon <contact@matt-mcmahon.com>
2020-10-16 00:24:57 -0400

    lib/terminal.ts no longer requires --allow-env

--- source/lib/terminal.ts
+++ source/lib/terminal.ts
@@ -1,6 +1,3 @@
-const getEnVar = (varName: string, varDefault: string) =>
-  Number.parseInt(Deno.env.get(varName) || varDefault, 10);
-
 export const { columns, rows } = typeof Deno?.consoleSize === "function"
   ? Deno.consoleSize(Deno.stdout.rid)
-  : { columns: getEnVar("COLS", "80"), rows: getEnVar("ROWS", "43") };
+  : { columns: 80, rows: 43 };
